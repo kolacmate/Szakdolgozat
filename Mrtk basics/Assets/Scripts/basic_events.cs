d9d03c8ca003e2660c5c043978f4869e42c9aab6
kolacmate <kolacmate@gmail.com>
2021-03-23 19:47:38 +0100

    Mrtk basics

---
+++ Mrtk basics/Assets/Scripts/basic_events.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basic_events : MonoBehaviour ,IMixedRealityPointerHandler
+{
+    public void Proba()
+    {
+        Debug.Log("Gomb megnyomva");
+        GameObject cube = GameObject.Find("Teglalap");
+        var cubeRenderer = cube.GetComponent<Renderer>();
+        cubeRenderer.material.color = Color.white;
+    }
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("OnPointerClicked");
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        //Debug.Log("OnPointerDragged");
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("OnPointerUp");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Mrtk basics/Assets/Scripts/gaze.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gaze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(
+                Camera.main.transform.position,
+                Camera.main.transform.forward,
+                out hitInfo,
+                20.0f,
+                Physics.DefaultRaycastLayers))
+        {
+            // If the Raycast has succeeded and hit a hologram
+            // hitInfo's point represents the position being gazed at
+            // hitInfo's collider GameObject represents the hologram being gazed at
+            Debug.Log("Looked at something");
+        }
+    }
+}
