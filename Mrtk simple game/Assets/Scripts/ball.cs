8c61c8a75edc7db25793b27a13204253a9d787b5
kolacmate <kolacmate@gmail.com>
2021-03-27 19:43:36 +0100

    Simple game

---
+++ Mrtk simple game/Assets/Scripts/ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public Transform target;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            //Destroy(other.gameObject);
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.constraints = RigidbodyConstraints.None;
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            Debug.Log("Elso");
+        }
+        if (other.gameObject.layer == 11)
+        {
+            //Destroy(other.gameObject);
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.constraints = RigidbodyConstraints.None;
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX;
+            Debug.Log("Masoidk");
+        }
+        if (other.gameObject.layer == 9)
+        {
+            player.transform.position = target.transform.position;
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            rigidbody.constraints = RigidbodyConstraints.None;
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+        }
+        if (other.gameObject.layer == 10)
+        {
+            player.transform.position = target.transform.position;
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+            rigidbody.constraints = RigidbodyConstraints.None;
+            rigidbody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+        }
+    }
+}
